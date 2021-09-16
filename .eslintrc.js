0c1e4eb5f3b8c0a8257540be57523660c49ec80f
Sergey Victorovich <svitch@gmail.com>
2021-09-16 13:18:15 +0300

    eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,28 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
-    '@vue/typescript/recommended',
+    '@vue/typescript',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'no-restricted-globals': 'off',
+    'linebreak-style': 'off',
+    'class-methods-use-this': 'off',
+    'new-cap': 'off',
+    'no-trailing-spaces': 'off',
+    'no-param-reassign': 'off',
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    }
+  }
 };
